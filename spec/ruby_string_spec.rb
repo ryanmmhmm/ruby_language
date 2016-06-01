81ee3c7e680add3189bb9b0516b88427f801212b
Ryan Magowan <ryanm@nulogy.com>
2016-06-01 10:32:53 -0400

    adds all methods available to String as describe blocks in test

---
+++ spec/ruby_string_spec.rb
@@ -0,0 +1,295 @@
+require 'ruby_string'
+
+RSpec.describe RubyString do
+
+  xdescribe "new" do
+  end
+
+  xdescribe "try_convert" do
+  end
+
+  xdescribe "string % /argument" do
+  end
+
+  xdescribe "string * integer" do
+  end
+
+  xdescribe "string + other_string" do
+  end
+
+  xdescribe "mutable strings" do
+  end
+
+  xdescribe "frozen strings" do
+  end
+
+  xdescribe "string << integer" do
+  end
+
+  xdescribe "string << object" do
+  end
+
+  xdescribe "string == object" do
+  end
+
+  xdescribe "string === object" do
+  end
+
+  xdescribe "string =~ object" do
+  end
+
+  xdescribe "string[things]" do
+  end
+
+  xdescribe "string ascii_only" do
+  end
+
+  xdescribe "string.b" do
+  end
+
+  xdescribe "string.bytes" do
+  end
+
+  xdescribe "bytesize" do
+  end
+
+  xdescribe "byteslice" do
+  end
+
+  xdescribe "capitalize" do
+  end
+
+  xdescribe "casecmp" do
+  end
+
+  xdescribe "center" do
+  end
+
+  xdescribe "chars" do
+  end
+
+  xdescribe "chomp" do
+  end
+
+  xdescribe "chomp!" do
+  end
+
+  xdescribe "chop" do
+  end
+
+  xdescribe "chr" do
+  end
+
+  xdescribe "clear" do
+  end
+
+  xdescribe "codepoints" do
+  end
+
+  xdescribe "concat" do
+  end
+
+  xdescribe "count" do
+  end
+
+  xdescribe "crypt" do
+  end
+
+  xdescribe "delete" do
+  end
+
+  xdescribe "downcase" do
+  end
+
+  xdescribe "dump" do
+  end
+
+  xdescribe "each_byte" do
+  end
+
+  xdescribe "each_char" do
+  end
+
+  xdescribe "each_codepoint" do
+  end
+
+  xdescribe "each_line" do
+  end
+
+  xdescribe "empty?" do
+  end
+
+  xdescribe "encode" do
+  end
+
+  xdescribe "encoding" do
+  end
+
+  xdescribe "end_with?" do
+  end
+
+  xdescribe "eql?" do
+  end
+
+  xdescribe "force_encoding" do
+  end
+
+  xdescribe "freeze" do
+  end
+
+  xdescribe "getbyte" do
+  end
+
+  xdescribe "gsub" do
+  end
+
+  xdescribe "hash" do
+  end
+
+  xdescribe "hex" do
+  end
+
+  xdescribe "include?" do
+  end
+
+  xdescribe "index" do
+  end
+
+  xdescribe "replace" do
+  end
+
+  xdescribe "insert" do
+  end
+
+  xdescribe "inspect" do
+  end
+
+  xdescribe "intern" do
+  end
+
+  xdescribe "length" do
+  end
+
+  xdescribe "length" do
+  end
+
+  xdescribe "lines" do
+  end
+
+  xdescribe "ljust" do
+  end
+
+  xdescribe "lstrp" do
+  end
+
+  xdescribe "match" do
+  end
+
+  xdescribe "next" do
+  end
+
+  xdescribe "oct" do
+  end
+
+  xdescribe "ord" do
+  end
+
+  xdescribe "partition" do
+  end
+
+  xdescribe "prepend" do
+  end
+
+  xdescribe "replace" do
+  end
+
+  xdescribe "reverse" do
+  end
+
+  xdescribe "rindex" do
+  end
+
+  xdescribe "rindex" do
+  end
+
+  xdescribe "rjust" do
+  end
+
+  xdescribe "rpartition" do
+  end
+
+  xdescribe "rstrip" do
+  end
+
+  xdescribe "scan" do
+  end
+
+  xdescribe "scrub" do
+  end
+
+  xdescribe "setbyte" do
+  end
+
+  xdescribe "slice" do
+  end
+
+  xdescribe "split" do
+  end
+
+  xdescribe "squeeze" do
+  end
+
+  xdescribe "start_with?" do
+  end
+
+  xdescribe "strip" do
+  end
+
+  xdescribe "sub" do
+  end
+
+  xdescribe "succ" do
+  end
+
+  xdescribe "sum" do
+  end
+
+  xdescribe "swapcase" do
+  end
+
+  xdescribe "to_c" do
+  end
+
+  xdescribe "to_f" do
+  end
+
+  xdescribe "to_i" do
+  end
+
+  xdescribe "to_r" do
+  end
+
+  xdescribe "to_s" do
+  end
+
+  xdescribe "to_sym" do
+  end
+
+  xdescribe "tr" do
+  end
+
+  xdescribe "tr_s" do
+  end
+
+  xdescribe "unpack" do
+  end
+
+  xdescribe "upcase" do
+  end
+
+  xdescribe "upto" do
+  end
+
+  xdescribe "valid_encoding?" do
+  end
+end
