70c40056455b2551e1440496a239c7ceb44453e2
Ryan Magowan <ryanm@nulogy.com>
2016-06-06 16:35:23 -0400

    adds all public API methods to enumerable_spec.rb

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,171 @@
+RSpec.describe Enumerable do
+  context "Public Instance Methods" do
+    xdescribe "#all?" do
+    end
+
+    xdescribe "#any?" do
+    end
+
+    xdescribe "#chunk" do
+    end
+
+    xdescribe "#chunk_while" do
+    end
+
+    xdescribe "#collect" do
+    end
+
+    xdescribe "#collect_count" do
+    end
+
+    xdescribe "#count" do
+    end
+
+    xdescribe "#cycle" do
+    end
+
+    xdescribe "#detect" do
+    end
+
+    xdescribe "#drop" do
+    end
+
+    xdescribe "#drop_while" do
+    end
+
+    xdescribe "#each_cons" do
+    end
+
+    xdescribe "#each_entry" do
+    end
+
+    xdescribe "#each_entry" do
+    end
+
+    xdescribe "#each_slice" do
+    end
+
+    xdescribe "#each_with_object" do
+    end
+
+    xdescribe "#entries" do
+    end
+
+    xdescribe "#find" do
+    end
+
+    xdescribe "#find_all" do
+    end
+
+    xdescribe "#find_index" do
+    end
+
+    xdescribe "#first" do
+    end
+
+    xdescribe "#flat_map" do
+    end
+
+    xdescribe "#grep" do
+    end
+
+    xdescribe "#grep_v" do
+    end
+
+    xdescribe "#group_by" do
+    end
+
+    xdescribe "#include?" do
+    end
+
+    xdescribe "#inject" do
+    end
+
+    xdescribe "#lazy" do
+    end
+
+    xdescribe "#map" do
+    end
+
+    xdescribe "#max" do
+    end
+
+    xdescribe "#max_by" do
+    end
+
+    xdescribe "#member?" do
+    end
+
+    xdescribe "#max" do
+    end
+
+    xdescribe "#max_by" do
+    end
+
+    xdescribe "#member?" do
+    end
+
+    xdescribe "#min" do
+    end
+
+    xdescribe "#min_by" do
+    end
+
+    xdescribe "#minmax" do
+    end
+
+    xdescribe "#minmax_by" do
+    end
+
+    xdescribe "#none?" do
+    end
+
+    xdescribe "#one?" do
+    end
+
+    xdescribe "#partition" do
+    end
+
+    xdescribe "#reduce" do
+    end
+
+    xdescribe "#reject" do
+    end
+
+    xdescribe "#reverse_each" do
+    end
+
+    xdescribe "#select" do
+    end
+
+    xdescribe "#slice_after" do
+    end
+
+    xdescribe "#slice_before" do
+    end
+
+    xdescribe "#slice_when" do
+    end
+
+    xdescribe "#sort" do
+    end
+
+    xdescribe "#sort_by" do
+    end
+
+    xdescribe "#take" do
+    end
+
+    xdescribe "#take_while" do
+    end
+
+    xdescribe "#to_a" do
+    end
+
+    xdescribe "#to_h" do
+    end
+
+    xdescribe "#zip" do
+    end
+  end
+end
