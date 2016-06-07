a41754086e84288aba36c8fdb4e7a4d61cc3fac0
Ryan Magowan <ryanm@nulogy.com>
2016-06-06 20:09:13 -0400

    adds describe blocks for all Hash Public API

---
+++ spec/hash_spec.rb
@@ -0,0 +1,206 @@
+Rspec.describe Hash do
+  context "Public Class Methods" do
+    xdescribe "Hash[ ]" do
+    end
+
+    xdescribe ".new" do
+    end
+
+    xdescribe ".try_convert()" do
+    end
+  end
+
+  context "Public Instance Methods" do
+    xdescribe " < " do
+    end
+
+    xdescribe " <= " do
+    end
+
+    xdescribe " == " do
+    end
+
+    xdescribe " > " do
+    end
+
+    xdescribe " >= " do
+    end
+
+    xdescribe "hsh[key]" do
+    end
+
+    xdescribe "hsh[key] = value" do
+    end
+
+    xdescribe "#any?" do
+    end
+
+    xdescribe "#assoc()" do
+    end
+
+    xdescribe "#clear" do
+    end
+
+    xdescribe "#compare_by_identity" do
+    end
+
+    xdescribe "#compare_by_identity?" do
+    end
+
+    xdescribe "#default" do
+    end
+
+    xdescribe "#default = obj" do
+    end
+
+    xdescribe "#default_proc" do
+    end
+
+    xdescribe "#default_proc = " do
+    end
+
+    xdescribe "#delete(key)" do
+    end
+
+    xdescribe "#delete_if" do
+    end
+
+    xdescribe "#dig(key)" do
+    end
+
+    xdescribe "#each" do
+    end
+
+    xdescribe "#each_pair" do
+    end
+
+    xdescribe "#each_key" do
+    end
+
+    xdescribe "#each_value" do
+    end
+
+    xdescribe "#empty?" do
+    end
+
+    xdescribe "#fetch(key)" do
+    end
+
+    xdescribe "#fetch_values" do
+    end
+
+    xdescribe "#flatten" do
+    end
+
+    xdescribe "#has_key?" do
+    end
+
+    xdescribe "#has_value?" do
+    end
+
+    xdescribe "#hash" do
+    end
+
+    xdescribe "#include?" do
+    end
+
+    xdescribe "#to_s" do
+    end
+
+    xdescribe "#inspect" do
+    end
+
+    xdescribe "#invert" do
+    end
+
+    xdescribe "#keep_if" do
+    end
+
+    xdescribe "#key(value)" do
+    end
+
+    xdescribe "#key(key)" do
+    end
+
+    xdescribe "#key?(key)" do
+    end
+
+    xdescribe "#keys" do
+    end
+
+    xdescribe "#length" do
+    end
+
+    xdescribe "#member?(key)" do
+    end
+
+    xdescribe "#merge(a_hash)" do
+      xit "merge combines the contents of two hashes" do
+      end
+
+      xit "merge! mutates the hash in place" do
+      end
+    end
+
+    xdescribe "#rassoc(obj)" do
+    end
+
+    xdescribe "#rehash" do
+    end
+
+    xdescribe "#reject" do
+      xit "returns a new hash of entries for which the hash returned false" do
+      end
+
+      xit "reject! mutates the object in place" do
+      end
+    end
+
+    xdescribe "#replace(a_hash)" do
+    end
+
+    xdescribe "#select" do
+      xit "returns a new hash of entries for which the hash returned true" do
+      end
+
+      xit "select! mutates the object in place" do
+      end
+    end
+
+    xdescribe "#shift" do
+    end
+
+    xdescribe "#size" do
+    end
+
+    xdescribe "#store(key, value)" do
+    end
+
+    xdescribe "#to_a" do
+    end
+
+    xdescribe "#to_h" do
+    end
+
+    xdescribe "#to_hash" do
+    end
+
+    xdescribe "#to_proc" do
+    end
+
+    xdescribe "#to_s" do
+    end
+
+    xdescribe "#update(a_hash)" do
+    end
+
+    xdescribe "#value?(value)" do
+    end
+
+    xdescribe "#values" do
+    end
+
+    xdescribe "#values_at(key, ...)" do
+    end
+  end
+end
